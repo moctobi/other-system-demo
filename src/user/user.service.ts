24e229609463a6496780bd73336edf8f12b46a2b
david8386 <buikhoa535467@gmail.com>
2025-10-14 17:12:44 +0700

    Add @faker-js/faker dependency and update UserService to use faker for generating user data

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { fakerVI } from "@faker-js/faker";
 
 @Injectable()
 export class UserService {
   getUser(username: string) {
     return {
       data: {
-        fullname: "TRANIE",
+        fullname: fakerVI.person.fullName(),
         username,
-        phone: true,
-        vip: "VIP0",
+        phone: fakerVI.helpers.arrayElement([true, false]),
+        vip: fakerVI.helpers.arrayElement(["VIP0", "VIP1", "VIP2", "VIP3"]),
         deposits: null,
       },
       message: "Thành công",
